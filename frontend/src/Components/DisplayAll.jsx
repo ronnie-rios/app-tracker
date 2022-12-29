9cd87a4f4983f43a94d5cd9eb4a17ae64b91bfd0
Ronnie Rios <ronniesmail14@gmail.com>
2022-12-29 15:18:15 -0600

    edit form

--- frontend/src/Components/DisplayAll.jsx
+++ frontend/src/Components/DisplayAll.jsx
@@ -1,15 +1,16 @@
 import { useEffect } from 'react';
 import { useState } from 'react';
+import { EditApp } from './EditApp';
 
 const URL = 'http://localhost:7001/applications'
 export const DisplayAll = () => {
     const [appsData, setAppsData] = useState([]);
+    const [edit, setEdit] = useState(false)
 
     const getAllApps = async () => {
         const response = await fetch(URL);
         const data = await response.json();
         setAppsData(data);
-        console.log(data)
     }
     useEffect(()=> {
         getAllApps()
@@ -21,12 +22,15 @@ export const DisplayAll = () => {
                 <h3>role: {app.jobRole}</h3>
                 <h3>Company: {app.company}</h3>
                 <h4>technologies: {app.technologies}</h4>
+                <button onClick={()=>setEdit(true)}>edit?</button>
+                {edit && <EditApp appData={appsData}/>}
             </div>
         )
     }
   return (
     <>
         {appsData.map((app) => renderContent(app))}
+       
     </>
   )
 }
--- frontend/src/Components/EditApp.jsx
+++ frontend/src/Components/EditApp.jsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import { useState } from 'react'
 
-export const EditApp = () => {
+export const EditApp = ({ appData }) => {
+  const [editForm, setEditForm] = useState({
+    interview: false,
+    phoneScreening: false,
+    accepted: false
+  })
+
+
   return (
-    <div>EditApp</div>
+    <div>
+      <h4>edit</h4>
+      <input type="radio" name='interview'  />
+    </div>
   )
 }
