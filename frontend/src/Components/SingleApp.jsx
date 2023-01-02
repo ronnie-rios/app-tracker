2397537d7d8ac34eccf14dd839e1637353b04e8b
Ronnie Rios <ronniesmail14@gmail.com>
2023-01-02 11:08:37 -0600

    put request and handler

--- frontend/src/Components/SingleApp.jsx
+++ frontend/src/Components/SingleApp.jsx
@@ -3,6 +3,7 @@ import { useParams } from 'react-router-dom';
 
 const SingleApp = () => {
     const [singleAppData, setSingleAppData] = useState([]);
+    const [formData, setFormData] = useState({});
     const id = useParams().id
   
     const getSingleApp = async () => {
@@ -14,19 +15,50 @@ const SingleApp = () => {
         getSingleApp()
     }, []);
 
-    
+    const editData = async () => {
+        try {
+          const response = await fetch(`http://localhost:7001/applications/${id}`, {
+            headers: { 
+              'Content-Type': 'application/json'
+            },
+            method: 'PUT',
+            body: JSON.stringify(formData)
+          });
+          const data = response.json();
+          return data;
+        } catch (error) {
+          console.log(error)
+        }
+      }
 
+      const formHandler = (e) => {
+        const name = e.target.name;
+        const value = e.target.value;
+        setFormData((prev)=>{
+          return { ...prev, [name]: value}
+        })
+      }
+    
+      const formSubmit = (e) => {
+        e.preventDefault();
+        editData();
+      }
   return (
-    <div>
-        <h4>{singleAppData.jobRole}</h4>
-        <h4>{singleAppData.company}</h4>
-        <h4>{singleAppData.technologies}</h4>
-        <h4>{singleAppData.fromWhere}</h4>
-        <h4>{singleAppData.interview}</h4>
-        <h4>{singleAppData.phoneScreening}</h4>
-        <h4>{singleAppData.accepted}</h4>
-        <h4>{singleAppData.denied}</h4>
-    </div>
+    <>
+        <div>
+            <h4>{singleAppData.jobRole}</h4>
+            <h4>{singleAppData.company}</h4>
+            <h4>{singleAppData.technologies}</h4>
+            <h4>{singleAppData.fromWhere}</h4>
+            <h4>{singleAppData.interview}</h4>
+            <h4>{singleAppData.phoneScreening}</h4>
+            <h4>{singleAppData.accepted}</h4>
+            <h4>{singleAppData.denied}</h4>
+        </div>
+        <form>
+
+        </form>
+    </>
   )
 }
 
