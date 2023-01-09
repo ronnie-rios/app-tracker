4e9e0ac8f1fa7626db67bccb0f1288771663fe00
Ronnie Rios <ronniesmail14@gmail.com>
2023-01-09 16:21:58 -0600

    icons and styled close button

--- frontend/src/Components/HomePage.jsx
+++ frontend/src/Components/HomePage.jsx
@@ -1,6 +1,7 @@
 import { useState} from 'react'
 import { DisplayAll } from './DisplayAll'
 import { PostApp } from './PostApp'
+import { AiOutlineCloseCircle } from 'react-icons/ai'
 
 const HomePage = () => {
   const [toggleRender, setToggleRender] = useState(false);
@@ -9,10 +10,13 @@ const HomePage = () => {
   return (
     <div>
         <div className='w-full max-w-xs py-6 px-4'>
-          <button onClick={()=> setViewForm(true)}>Add a job</button>
+          <button className='bg-green-600 rounded px-3 pb-1 mb-8 text-white hover:bg-green-800' onClick={()=> setViewForm(true)}>Add a job</button>
           {viewForm && 
-            <div className='shadow-md roundex px-8 pt=6 pb-8 mb-4'>
-              <button onClick={()=> setViewForm(false)}>nevermind</button>
+            <div className='shadow-md rounded relative px-8 pt-6 pb-8 mb-4'>
+              <AiOutlineCloseCircle 
+                className=' text-gray-800 hover:text-red-600 float-right'
+                onClick={()=> setViewForm(false)}
+              />
               <PostApp toggleRender={toggleRender} setToggleRender={setToggleRender}/>
             </div>
           }
