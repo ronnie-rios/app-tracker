a7848a94c52bf6c8b67b4d0704f0463c09583ed6
Ronnie Rios <ronniesmail14@gmail.com>
2023-01-24 12:00:44 -0600

    added more divs to profile

--- frontend/src/Components/Profile.jsx
+++ frontend/src/Components/Profile.jsx
@@ -33,31 +33,37 @@ const Profile = () => {
         )
     } else {
         return (
-            <div className="max-w-screen-xl mx-auto m-10">
+            <div className="max-w-screen-lg mx-auto m-10">
                 <div className='grid xl:grid-cols-3 lg:grid-cols-3 md:grid-cols-3 p-8 mb-8 md:gap-8'>
                     <div className='card col-span-1'>
                         <h3>Profile Section</h3>
                         <h3 className='card-title'>{username}</h3>
-                        <button className='bg-green' onClick={()=> navigate(`/profile/${token.id}`)}>Edit Profile</button>
                     </div>
                     <div className='col-span-2  border rounded-md border-sky-200 p-4'>
-                        <h3>Job looking for</h3>
+                        <h3 className='mb-2 text-xl font-semibold text-white'>I'm looking for. . .</h3>
+                        <h3>Description: </h3>
                         <h3>{jobDesc}</h3>
+                        <h3>Role I'm looking for: </h3>
                         <h3>{roleLookingFor}</h3>
+                    </div>
+                    <div className='col-start-2 col-span-2  border rounded-md border-sky-200 p-4'>
+                        <h3 className='mb-2 text-xl font-semibold text-white'>Work Experience</h3>
+                        <h3>My last role: </h3>
+                        <h3>-prev-title</h3>
+                        <h3>Years of experience: </h3>
                         <h3>{overallExperience}</h3>
 
                     </div>
-                <div className='col-start-2  col-span-2 border rounded-md border-sky-200 p-4'>
+                <div className='col-start-2 col-span-2 border rounded-md border-sky-200 p-4 '>
+                    <h3 className='mb-2 text-xl font-semibold text-white'>Skills</h3>
                     {skills && skills.map((skill) => {
                         return (
-                            <>
-                            <div key={skill.skillName}>
-                                <p>{skill.skillName}</p>
-                                <p>{skill.years}</p>
-                            </div>
-                            </>
+                            <ul key={skill.id} className='space-y-1 text-white list-disc list-inside m-2'>
+                                <li className='text-lg'>{skill.skillName} - {skill.years} year(s)</li>
+                            </ul>
                         )
                     })}
+                    <button className='btn' onClick={()=> navigate(`/profile/${token.id}`)}>Add skills</button>
                 </div>
                 </div>
             </div>
